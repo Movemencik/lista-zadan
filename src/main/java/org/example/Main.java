50def79a583cd4aa837a0ba3dec3856e8de4c5bf
Piotr Gibała <gibala.ptk@gmail.com>
2023-06-21 12:08:02 +0200

    Update.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,32 +16,31 @@ public class Main {
             System.out.println("3. Obecne zadania");
             System.out.println("4. Wyjdź");
 
-            int option = scanner.nextInt();
-            scanner.nextLine();
+            String option = scanner.nextLine();
 
             switch (option) {
-                case 1:
+                case "1":
                     System.out.print("Podaj treść zadania: ");
                     String name = scanner.nextLine();
                     Task task = new Task(name);
                     todoList.addTask(task);
                     System.out.println("Zadanie dodane.");
                     break;
-                case 2:
+                case "2":
                     System.out.print("Podaj numer zadania do usunięcia: ");
                     int index = scanner.nextInt();
                     scanner.nextLine();
                     todoList.removeTask(index);
                     System.out.println("Zadanie usunięte.");
                     break;
-                case 3:
+                case "3":
                     System.out.println("Obecne zadania:");
                     for (int i = 0; i < todoList.getTasks().size(); i++) {
                         Task t = todoList.getTasks().get(i);
                         System.out.println((i + 1) + ". " + t.getName());
                     }
                     break;
-                case 4:
+                case "4":
                     System.out.println("Zakończono program.");
                     System.exit(0);
                 default:
