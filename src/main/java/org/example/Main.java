6ae9270f0bdd2b6bcf9a919fde81a2abec313bf4
Piotr Gibała <gibala.ptk@gmail.com>
2023-06-28 12:38:35 +0200

    Update.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,51 +3,61 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         TodoList todoList = new TodoList();
 
         while (true) {
-            System.out.println("");
-            System.out.println("==== LISTA ZADAŃ ====");
-            System.out.println("Wybierz opcję:");
-            System.out.println("1. Dodaj zadanie");
-            System.out.println("2. Usuń zadanie");
-            System.out.println("3. Obecne zadania");
-            System.out.println("4. Wyjdź");
+            printMenu();
 
             String option = scanner.nextLine();
 
             switch (option) {
-                case "1":
+                case "1" -> {
                     System.out.print("Podaj treść zadania: ");
                     String name = scanner.nextLine();
                     Task task = new Task(name);
                     todoList.addTask(task);
                     System.out.println("Zadanie dodane.");
-                    break;
-                case "2":
-                    System.out.print("Podaj numer zadania do usunięcia: ");
-                    int index = scanner.nextInt();
-                    scanner.nextLine();
-                    todoList.removeTask(index);
-                    System.out.println("Zadanie usunięte.");
-                    break;
-                case "3":
-                    System.out.println("Obecne zadania:");
-                    for (int i = 0; i < todoList.getTasks().size(); i++) {
-                        Task t = todoList.getTasks().get(i);
-                        System.out.println((i + 1) + ". " + t.getName());
+                }
+                case "2" -> {
+                    if (todoList.getTasks().isEmpty()) {
+                        System.out.println("Brak zadań na liście.");
+                    } else {
+                        todoList.displayTasks();
+                        boolean istrue = false;
+                        int index = 0;
+                        while (!istrue) {
+                            try {
+                                System.out.print("Podaj numer zadania do usunięcia: ");
+                                String input = scanner.nextLine();
+                                index = Integer.parseInt(input);
+                                istrue = true;
+                            } catch (NumberFormatException e) {
+                                System.out.println("Niepoprawana wartość");
+                            }
+                        }
+                        todoList.removeTask(index);
                     }
-                    break;
-                case "4":
+                }
+                case "3" -> todoList.displayTasks();
+                case "4" -> {
                     System.out.println("Zakończono program.");
                     System.exit(0);
-                default:
-                    System.out.println("Niepoprawny wybór. Spróbuj ponownie.");
-                    break;
+                }
+                default -> System.out.println("Niepoprawny wybór. Spróbuj ponownie.");
             }
 
         }
+    }
+
+    private static void printMenu() {
+        System.out.println("==== LISTA ZADAŃ ====");
+        System.out.println("1. Dodaj zadanie");
+        System.out.println("2. Usuń zadanie");
+        System.out.println("3. Wyświetl zadania");
+        System.out.println("4. Wyjdź");
+        System.out.print("Wybierz opcję: ");
     }
 }
--- src/main/java/org/example/TodoList.java
+++ src/main/java/org/example/TodoList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TodoList {
-    private List<Task> tasks;
+    private final List<Task> tasks;
 
     public TodoList() {
         this.tasks = new ArrayList<>();
@@ -15,10 +15,28 @@ public class TodoList {
     }
 
     public void removeTask(int index) {
-        tasks.remove(index - 1);
+        if (index >= 1 && index <= tasks.size()) {
+            tasks.remove(index - 1);
+            System.out.println("Zadanie usunięte.");
+        } else {
+            System.out.println("Niepoprawny numer zadania.");
+        }
     }
 
     public List<Task> getTasks() {
         return tasks;
+    }
+
+    public void displayTasks() {
+        if (getTasks().isEmpty()) {
+            System.out.println("Brak zadań na liście.");
+        }
+        else {
+            System.out.println("==== Obecne zadania ====");
+            for (int i = 0; i < getTasks().size(); i++) {
+                Task t = getTasks().get(i);
+                System.out.println((i + 1) + ". " + t.getName());
+            }
+        }
     }
 }
